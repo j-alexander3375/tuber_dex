fad13a6d6df9f24a981f219503bd9832ca7a8029
Joshua <joshuaalexander0927@gmail.com>
2025-02-02 12:35:38 -0500

    Add files via upload

---
+++ hololive.py
@@ -0,0 +1,25 @@
+import streamlit as st
+
+holo = {
+    "Generation 0": ['Hoshimachi Suisei', 'Sakura Miko', 'AZKi', 'Tokino Sora', 'Robocosan'],
+    "Generation 1": ['Aki Rosenthal', 'Akai Haato', 'Natsuiro Matsuri'],
+    "Generation 2": ['Murasaki Shion', 'Nakiri Ayame', 'Yuzuki Choco', 'Ōzora Subaru',],
+    "GAMERS": ["Shirakami Fubuki", "Ōkami Mio", "Nekomata Okayu", "Inugami Korone"],
+    "Generation 3": ['Usada Pekora', 'Shiranui Flare', 'Shirogane Noel', 'Houshou Marine'],
+    "Generation 4": ['Amane Kanata', 'Tsunomaki Watame', 'Tokoyami Towa', 'Himemori Luna'],
+    "Generation 5": ['Yukihana Lamy', 'Momosuzu Nene', "Shishiro Botan", 'Omaru Polka'],
+    "holoX": ['La+ Darkness', "Takane Lui", "Hakui Koyori", "Kazama Iroha"],
+    "AREA15": ['Ayunda Risu', 'Moona Hoshinova', 'airani Iofifteen'],
+    "holoro": ["Kureiji Ollie", 'Anya Melfissa', 'Pavolia Reine'],
+    'holoh3ro': ['Vestia Zeta', 'Kaela Kovalskia', 'Kobo Kanaeru'],
+    'Myth': ['Mori Calliope', 'Takanashi Kiara', "Ninomae Ina'nis", 'Gawr Gura'],
+    'Promise': ['IRyS', 'Ouro Kronii', 'Nanashi Mumei', "Hakos Baelz"],
+    'Advent': ['Shiori Novella', "Koseki Bijou", 'Nerissa Ravencroft', 'Fuwawa and Mococo Abyssgard'],
+    'Justice': ['Elizabeth Rose Bloodflame', 'Cecilia Immergreen', 'Raora Panthera', 'Gigi Murin'],
+    'ReGLOSS': ['Hiodoshi Ao', 'Otonose Kanade', 'Ichijou Ririka', 'Juufuutei Raden', 'Todoroki Hajime'],
+    'FLOW GLOW': ['Isaki Riona', 'Koganei Niko', 'Mizumiya Su', 'Rindo Chihaya', 'Kikirara Vivi']
+}
+
+for key, item in holo.items():
+    for i in item:
+        st.write(f"Hololive {key}'s {i}")
