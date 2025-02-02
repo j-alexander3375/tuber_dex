10d30585bf09de1112a8c4e49a79122ff49c2065
Joshua <joshuaalexander0927@gmail.com>
2025-02-02 13:07:15 -0500

    Fix line formatting

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,9 @@
 import streamlit as st
 from hololive import holo
 
-st.write("Welcome to TuberDex!\nAs development continues I hope to add more Indie and other corporation related VTubers, though for now, we have all of HoloLive!\nBy the time this project comes to fruition, this can hopefully help you find your next oshi. :)")
+st.write("Welcome to TuberDex!")
+st.write("As development continues I hope to add more Indie and other corporation related VTubers, though for now, we have all of HoloLive!")
+st.write("By the time this project comes to fruition, this can hopefully help you find your next oshi. :)")
 
 for key, item in holo.items():
   for i in item:
