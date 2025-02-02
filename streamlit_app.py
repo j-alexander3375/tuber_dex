9b2c494a620fe9e998ad8d60bacc9cd54fbf88d2
Joshua <joshuaalexander0927@gmail.com>
2025-02-02 15:12:54 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ st.markdown("## As development continues I hope to add more Indie and other corp
 st.markdown("## By the time this project comes to fruition, this can hopefully help you find your next oshi. :)")
 st.write("")
 
- for key, item in holo.items():
+for key, item in holo.items():
   for i in item:
     st.write(f"Hololive {key}'s {i}")
 
