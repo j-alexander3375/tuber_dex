f7688626421dfd0eaa4d7ecbf68e765900d30833
Joshua <joshuaalexander0927@gmail.com>
2025-02-02 13:10:47 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,9 @@
 import streamlit as st
 from hololive import holo
 
-st.write("Welcome to TuberDex!")
-st.write("As development continues I hope to add more Indie and other corporation related VTubers, though for now, we have all of HoloLive!")
-st.write("By the time this project comes to fruition, this can hopefully help you find your next oshi. :)")
+st.markdown("# Welcome to TuberDex!")
+st.write("## As development continues I hope to add more Indie and other corporation related VTubers, though for now, we have all of HoloLive!")
+st.write("## By the time this project comes to fruition, this can hopefully help you find your next oshi. :)")
 st.write("")
 
 for key, item in holo.items():
